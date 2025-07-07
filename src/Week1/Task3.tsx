5f5fe6e9673a5dc8d7a32e4f3c3cca646e2308ec
D3190 <aadarsha.love@gmail.com>
2025-07-07 18:58:39 +0545

    homework update

---
+++ src/Week1/Task3.tsx
@@ -0,0 +1,14 @@
+
+
+const Task3 = () => {
+  return (
+    <div>
+      <p><strong>Paragraph</strong> is a collention of sentences that give out a <em>certain</em> meaning.</p>
+      <h1 className="text-9xl">h1</h1>
+      <br></br>
+      <h2 className="text-8xl">h2</h2>
+    </div>
+  )
+}
+
+export default Task3
