887fc553b5b21a649847be1176fe03e6d1407834
Ghost9841 <asubedi778@gmail.com>
2025-07-22 20:53:47 +0545

    Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { use, useEffect, useState } from "react"
-import MyApp, { MyButton } from "./stuffs/buttons"
+import { useEffect, useState } from "react"
+import ShowToDoPage from "./Todo/page/ShowToDoPage";
 import AppTaskComp from "./Todo/components/AppTaskComp";
 
 type Props = {
@@ -7,66 +7,19 @@ type Props = {
   name: string;
   avatar: string;
   id: string;
-}
+} 
 
 
-import Task1 from "./Week1/Task1"
-import Task2 from "./Week1/Task2"
-import Home from "./Week1/Home"
-import Task3 from "./Week1/Task3"
-import Task4 from "./Week1/Task4"
-import { BrowserRouter as Router , Routes, Route, Link  } from "react-router-dom";
+
+
 function App() {
-const [count, setcount] = useState(5);
-const [data, setData] = useState<Props[]>([]);
-function handleFetch (){
-  setcount(count + 10);
-}
-
-useEffect(() => {
-  fetch("https://67566fc611ce847c992cc7b5.mockapi.io/todo")
-    .then((response) => response.json())
-    .then((data) => {
-      setData(data.slice(0, 5)); // Limit to first 5 items
-    })
-    .catch((error) => console.error("Error fetching data:", error));
-}, [count]);
-
-
   return (
   
 
 
     <>
-
-      
-      <Router>
-       <div>
-       <nav>
-        <Link to="/Task3">Task3</Link> 
-        <Link to='/Task1'>Task1</Link>
-       <Link to="/Task2">Task2</Link>
-       <Link to="/Task4">Task4</Link>
-       <Link to="/">Home</Link></nav>
-       </div>
-       <Routes>
-
-
-       
-      
-      <Route path="/Task1" element={<Task1/>}></Route>
-      <Route path="/Task4"element={<Task4/>}></Route>
-      <Route path="/Task2" element={<Task2/>}></Route>
-      <Route path="/Task3" element={<Task3/>}></Route>
-      <Route index element={<Home/>}></Route>
-      
-      
-      
-      
-
-      </Routes>
-       
-      </Router>
+    <ShowToDoPage/>
+    <AppTaskComp/>
      
       
     </>
--- src/Todo/page/ShowToDoPage.tsx
+++ src/Todo/page/ShowToDoPage.tsx
@@ -1,22 +1,72 @@
-import AppTaskComp from "../components/AppTaskComp"
-import type { Props } from "../types/types"
+import { useEffect, useState } from "react";
 
 
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
-const ShowToDoPage = (props: Props) => {
+const ShowToDoPage = () => {
+  const [todos, setTodos] = useState<Todo[]>(()=>{
+    const savedTodos = localStorage.getItem('todos');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+  const [newTodo, setNewTodo] = useState<string>('');
   
+  useEffect(()=>  {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  },[todos]);
+
+  const addTodo = ():void => {
+    setTodos([...todos, {id: Date.now(),text:newTodo, completed: false}])
+    setNewTodo('')
+  }
+
+  const deleteTodo = (id: number): void => {
+    setTodos(todos.filter(todo => todo.id !==id));
+  }
+
   return (
     <>
-    <div>{props.name}</div>
-    <div>{props.height}</div>
-    <AppTaskComp/>
+    <div>
+      <h1 className="text-2xl flex py-4 items-center justify-center">Todo App</h1>
+      <div className="flex justify-between mx-12 items-center my-10 gap-4">
+        <input 
+        type="text"
+        value={newTodo}
+        onChange={(event)=>{setNewTodo(event.target.value)}}
+        placeholder="Add a new Todo"
+        className="rounded-lg shadow-lg p-6 w-full border-2 border-blue-500 hover:border-blue-400"
+         />
+         <button 
+         className="p-4 border-2 border-black rounded-2xl
+          hover:text-white
+          hover:bg-blue-500 cursor-pointer"
+          onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map(todo=> {
+          return(
+            <li key={todo.id} className={todo.completed ? 'bg-red-500' :  "bg-green-500"}>
+            <span>{todo.text}</span>
+            <button 
+            onClick={()=> deleteTodo(todo.id)} 
+            className="
+            p-4 border-2 border-black rounded-2xl
+          hover:text-white
+          hover:bg-red-500 cursor-pointer
+            ">Delete</button>
+          </li>
+      )
+        })}
+        </ul>
+    </div>
     </>
   )
 }
 
 export default ShowToDoPage
-
-
 
 
 
